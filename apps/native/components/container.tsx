cd3d0a1d85270407f59457a142bba43b31239210
d1rshan <darshan.paccha@gmail.com>
2026-03-28 00:38:01 +0530

    fix(native): remove gap above tab bar

--- apps/native/components/container.tsx
+++ apps/native/components/container.tsx
@@ -3,7 +3,11 @@ import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export function Container({ children }: { children: React.ReactNode }) {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView edges={["top", "left", "right"]} style={styles.container}>
+      {children}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
